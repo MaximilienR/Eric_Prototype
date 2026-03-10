edd686213fadc62c8e89899cf49e8e9bd94d454a
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2026-03-10 12:26:04 +0100

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,43 +4,35 @@ import logo from "../../assets/logo_footer.svg";
 
 export default function Footer() {
   return (
-   <div className="flex flex-col items-center md:flex-row md:justify-center md:gap-24 text-center md:text-left bg-black">
+    <footer className="bg-black text-gray-400">
+      {/* Conteneur principal du footer */}
+      <div className="flex flex-col items-center md:flex-row md:justify-center md:gap-24 text-center md:text-left p-6">
 
-  {/* Logo */}
-  <div className="w-48 flex justify-center md:justify-start">
-    <img
-      src={logo}
-      alt="Logo"
-      className="w-32 h-32 rounded-full"
-    />
-  </div>
+        {/* Logo */}
+        <div className="w-48 flex justify-center md:justify-start mb-6 md:mb-0">
+          <img src={logo} alt="Logo" className="w-32 h-32 rounded-full" />
+        </div>
 
-  {/* Navigation */}
-  <div className="w-48 flex flex-col space-y-4 text-lg  text-gray-400">
-    <Link to="/" className="hover:text-gray-400 transition">
-      Accueil
-    </Link>
+        {/* Navigation */}
+        <div className="w-48 flex flex-col items-start space-y-4 text-lg">
+          <Link to="/" className="hover:text-gray-400 transition">Accueil</Link>
+          <Link to="/about" className="hover:text-gray-400">À propos</Link>
+          <Link to="/contact" className="hover:text-gray-400 transition">Contact</Link>
+        </div>
 
-    <Link to="/about" className="hover:text-gray-400">
-      À propos
-    </Link>
+        {/* Infos */}
+        <div className="w-48 flex flex-col items-start space-y-4 text-lg">
+          <Link to="/mention" className="hover:text-gray-400">Mention légale</Link>
+          <Link to="/confidentiel" className="hover:text-gray-400 whitespace-nowrap">
+            Politique de confidentialité
+          </Link>
+        </div>
+      </div>
 
-    <Link to="/contact" className="hover:text-gray-400 transition">
-      Contact
-    </Link>
-  </div>
-
-  {/* Infos */}
-  <div className="w-48 flex flex-col space-y-4 text-lg text-gray-400">
-    <Link to="/mention" className="hover:text-gray-400 ">
-      Mention légale
-    </Link>
-
-    <Link to="/confidentiel" className="hover:text-gray-400">
-      Politique de confidentialité
-    </Link>
-  </div>
-
-</div>
+      {/* Texte de copyright */}
+      <div className="text-center text-gray-500 text-sm py-4 border-t border-gray-800">
+        © 2026 Eric Colin – Tous droits réservés
+      </div>
+    </footer>
   );
 }
